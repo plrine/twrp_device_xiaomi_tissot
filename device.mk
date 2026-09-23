ff914224cbdb556c7b0095ad3b931a32a736200e
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-09-23 17:10:46 +0000

    tissot: Drop broken copy files rules from device.mk
    * Remove invalid PRODUCT_COPY_FILES rules that attempted to copy
      crash_dump, gatekeeper, and keymaster binaries out of $(OUT_DIR).
    * crash_dump is an APEX runtime component that is neither used nor
      present in recovery (TW_EXCLUDE_APEX is enabled).
    * Hardware keymaster and gatekeeper services are already provided as
      QTI proprietary prebuilts under recovery/root/system/bin/ and
      recovery/root/system/lib64/hw/.
    
    Change-Id: Idbc3e45e03e303f72df88a9a604c5e066616da65

--- device.mk
+++ device.mk
@@ -32,34 +32,6 @@ PRODUCT_PACKAGES += \
     qcom_decrypt_fbe \
     qcom_decrypt
 
-# Debug
-PRODUCT_PACKAGES += \
-    crash_dump \
-    libprocinfo.recovery
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/tissot/system/apex/com.android.runtime/bin/crash_dump32:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/crash_dump32 \
-    $(OUT_DIR)/target/product/tissot/system/apex/com.android.runtime/bin/crash_dump64:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/crash_dump64
-
-# Gatekeeper
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/tissot/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.gatekeeper@1.0-service \
-    $(OUT_DIR)/target/product/tissot/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/hw/android.hardware.gatekeeper@1.0-impl.so
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/tissot/system/lib64/libkeymaster3device.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libkeymaster3device.so \
-    $(OUT_DIR)/target/product/tissot/vendor/bin/hw/android.hardware.keymaster@3.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.keymaster@3.0-service \
-    $(OUT_DIR)/target/product/tissot/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/hw/android.hardware.keymaster@3.0-impl.so
-
 # LED
 PRODUCT_PACKAGES += \
     charger_led
